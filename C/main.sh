5cab2885b3e54fe1d013fd8ff404a1259cdb23de
Khanor <73908732+JonP-dev@users.noreply.github.com>
2023-12-01 11:15:45 +0100

    Carriage return removal, updated E-code for C

--- C/main.sh
+++ C/main.sh
@@ -10,7 +10,7 @@ LOGS_DIR="${SCRIPT_DIR}/logs"
 DATA_DIR="${SCRIPT_DIR}/data"
 
 # importing our utilities
-# source "${UTIL_DIR}/get.page.sh"
+# source "${UTIL_DIR}/get_page.sh"
 source "${UTIL_DIR}/html.sh"
 source "${UTIL_DIR}/math.sh"
 
@@ -62,20 +62,27 @@ if [[ ! -f  "${DATA_DIR}/places.txt" ]]; then
 fi
 
 ### Download the webpage to our local machine.
-curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > step_1_page.html.txt
+### I prefix the files with "step_x" to get them in order in the folder,
+### and to simplify hunting for eventual errors. - Jon
+curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > step_1_page_html.txt
 
 ### Removing whitespace characters (also tabs).
-cat "step_1_page.html.txt" | tr -d '\n\t' > "step_2_page_one_line.html.txt"
+cat "step_1_page_html.txt" | tr -d '\n\t' > "step_2_page_one_line_html.txt"
 ### ^ "-d" stands for delete, deletes any of the following characters.
 
 ### Everything is on the same line; extracting our table in the third line (putting whitespace characters around it).
-sed -E 's|<table class="sortable wikitable">|\n<table class="sortable wikipage">|' "step_2_page_one_line.html.txt" |
-    sed -E 's|(</table>)|</table>\1\n|g' > "step_3_page_table_newline.html.txt"
+sed -E 's|<table class="sortable wikitable">|\n<table class="sortable wikipage">|' "step_2_page_one_line_html.txt" |
+    sed -E 's|(</table>)|</table>\1\n|g' > "step_3_page_table_newline_html.txt"
 ### ^ sed stands for "string editor."
 ### ^ "-E" stands for extended syntax, but we might not actually need it here.
 
-sed -n '3p' "step_3_page_table_newline.html.txt" > "step_4_table_only.txt"
+sed -n '3p' "step_3_page_table_newline_html.txt" > "step_4_table.txt"
 ### ^ "-n" stands for not printing the current line.
+
+### Getting images to show up not only in the "live server" extension for VSC, but also in an open HTML-file.
+### This is done by replacing the "//" with "https://" so opening the HTML-file will look on the internet for the images,
+### rather than on our computer. Also creating a new intermediate file from the "step_4"-file to copy into. - Jon
+sed -E 's|//upload|https://upload|g' "step_4_table.txt" > "step_5_table_with_images.txt"
 
 ### Inserting a boilerplate HTML5 template here, with some light customization and additions.
 page_template='
@@ -93,7 +100,7 @@ page_template='
 
         <h1>My fancy municipalities table</h1>
         <hr>
-        '"$(cat "step_4_table_only.txt")"'
+        '"$(cat "step_5_table_with_images.txt")"'
     </body>
 </html>
 '
